66e224f48d6250cd178e07f83f49a002e3a42a62
Amr Ashebo <amr8644@gmail.com>
2021-11-17 17:09:41 +0300

    Counter

---
+++ app.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+
+const items = document.querySelectorAll(".deadline-format h4");
+const futureDate = new Date(2022, 1, 1);
+
+const remainingTime = () => {
+  const date = new Date().getTime();
+  const time = futureDate - date;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+
+  let days = time / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((time % oneDay) / oneHour);
+  let minutes = Math.floor((time % oneHour) / oneMin);
+  let seconds = Math.floor((time % oneMin) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+  const format = (item) => {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  };
+
+  items.forEach((item, index) => {
+    item.innerHTML = values[index];
+  });
+};
+
+let countDown = setInterval(remainingTime, 1000);
